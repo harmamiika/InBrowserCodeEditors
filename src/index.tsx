1e14d8bc777b59a4d0415d7d91c389e8ec8602c1
Miika Härmä <harmamiika@gmail.com>
2021-07-11 21:01:55 +0300

    finished refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue={'type here'}
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
