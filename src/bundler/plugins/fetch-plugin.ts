ae1c6a9bcf3922efb92e0e832a1f54ec10e295f1
Miika Härmä <harmamiika@gmail.com>
2021-09-22 23:26:14 +0300

    added image

--- src/bundler/plugins/fetch-plugin.ts
+++ src/bundler/plugins/fetch-plugin.ts
@@ -18,59 +18,38 @@ export const fetchPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('i ran but didnt do a thing');
-        return null;
-      });
-
-      build.onLoad({ filter: /.css$/ }, async (args: any) => {
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
 
-        // if it is, return it immediately
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
-
         const escaped = data
           .replace(/\n/g, '')
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-
         const contents = `
-              const style = document.createElement('style');
-              style.innerText = '${escaped}';
-              document.head.appendChild(style)
-              `;
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
-
-        // store response in cache
         await fileCache.setItem(args.path, result);
 
         return result;
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        // if it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
-
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
@@ -78,8 +57,6 @@ export const fetchPlugin = (inputCode: string) => {
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
-
-        // store response in cache
         await fileCache.setItem(args.path, result);
 
         return result;
